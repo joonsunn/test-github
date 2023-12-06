743847053fa1d86bf3dc59ea7729d4a50fedb807
Foo Joon Sunn <joonsunn.foo@gmail.com>
2023-12-06 21:43:20 +0800

    feat: add develop route

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response, request } from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', async (request:Request, response:Response) => {
-	return response.json({message: 'You have reached home route'})
-})
+app.get("/", async (request: Request, response: Response) => {
+    return response.json({ message: "You have reached home route" });
+});
 
-app.get('/branch', async (request, response) => {
-	return response.json({message: 'You are on branch route'})
-})
+app.get("/branch", async (request, response) => {
+    return response.json({ message: "You are on branch route" });
+});
 
-export default app
+app.get("/develop", async (request, response) => {
+    return response.json({ message: "You are on develop route" });
+});
+
+export default app;
